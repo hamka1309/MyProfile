b4597b0371e54ddb664002cab75f9c5414bbf4e5
hamka1309 <hamika1309@gmail.com>
2019-06-13 19:15:13 +0700

    -chỉnh giao diẹn của choose_cycle

--- app/src/main/java/com/example/app1/fragment/ChooseCycleFragment.java
+++ app/src/main/java/com/example/app1/fragment/ChooseCycleFragment.java
@@ -7,18 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app1.MainActivity;
 import com.example.app1.PeriodActivity;
 import com.example.app1.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChooseCycleFragment extends Fragment implements View.OnClickListener {
 private Button btNext;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_choose_cycle, container, false);
+//        String [] values =
+//                {"1","2","3","4","5","6","7"};
+//        Spinner spinner = (Spinner) v.findViewById(R.id.spinner_day);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+//        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+//        spinner.setAdapter(adapter);
         return v;
     }
 
@@ -27,6 +43,8 @@ private Button btNext;
         super.onActivityCreated(savedInstanceState);
         btNext = getActivity().findViewById(R.id.bt_next);
         btNext.setOnClickListener(this);
+
+
     }
 
     @Override
